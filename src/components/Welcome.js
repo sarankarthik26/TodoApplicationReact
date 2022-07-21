33f4986e667c399d4f4e93ef809df4beeb69d9ff
Saran Karthik E <sarankarthik@Sarans-MacBook-Pro.local>
2022-07-21 14:12:25 +0530

    Completed my welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,15 +1,14 @@
 import { makeStyles } from '@mui/styles';
 import { Box } from '@mui/system';
-import { useNavigate } from "react-router-dom";
 import TodoAppBar from './TodoAppBar';
 import WelcomeBody from './WelcomeBody';
 import WelcomeImages from './WelcomeImages';
 
 const useStyles = makeStyles({
     welcomePage: {
-        backgroundImage: `url(${process.env.PUBLIC_URL + "todoWelcome.jpg"})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
+        // backgroundImage: `url(${process.env.PUBLIC_URL + "todoWelcome.jpg"})`,
+        // backgroundRepeat: 'no-repeat',
+        // backgroundSize: 'cover',
         minHeight: "100vh",
         width: "100%",
         margin: "0 auto",
@@ -17,24 +16,31 @@ const useStyles = makeStyles({
         justifyContent: "center",
         alignItems: "center",
         // filter: "blur(1px)",
+        backgroundColor: "rgb(0,0,0,0.4)",
+
     },
+    background: {
+        backgroundImage: `url(${process.env.PUBLIC_URL + "todoWelcome.jpg"})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+    }
 });
 
 const WelcomePage = ({ UserName }) => {
-    const navigate = useNavigate();
-    if (UserName) { navigate("/todos") }
-
     const classes = useStyles();
 
     return (
-        <div className={classes.welcomePage} >
-            <TodoAppBar />
-            <Box display={"flex"} flexDirection={{ xs: "column-reverse", md: "row" }} justifyContent={"center"} alignItems={"center"} bgcolor={"rgb(0,0,0,0.5)"}>
-                <Box paddingRight={{ xs: "0px", md: "30px" }}>
-                    <WelcomeBody />
+        <div className={classes.background}>
+            <div className={classes.welcomePage} >
+                <TodoAppBar />
+                <Box display={"flex"} flexDirection={{ xs: "column-reverse", md: "row" }} justifyContent={"center"} alignItems={"center"} bgcolor={"rgb(0,0,0,0.5)"}
+                    sx={{ borderRadius: "12px" }}>
+                    <Box paddingRight={{ xs: "0px", md: "30px" }}>
+                        <WelcomeBody />
+                    </Box>
+                    <WelcomeImages />
                 </Box>
-                <WelcomeImages />
-            </Box>
+            </div>
         </div>
     );
 }
